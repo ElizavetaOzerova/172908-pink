30a573e4462568f218b5d4b105d3b7cc94aacf36
Elizaveta <elizabeth11@bk.ru>
2018-01-29 17:19:38 +0300

    add basic styles

---
+++ source/js/map.js
@@ -0,0 +1,25 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938631, 30.323055],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/icon-map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [35, 35],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
